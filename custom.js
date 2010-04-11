bfa01ab5699549380ed48b1226f3fb91712edb11
Joshua Clayton <jclayton@thoughtbot.com>
2010-04-11 11:33:35 -0400

    Wait until doc is loaded before centering images properly

--- custom.js
+++ custom.js
@@ -1,5 +1,6 @@
-$(".content img").each(function() {
-  var badMarginTop = $(this).parents(".content").css("marginTop");
-  $(this).parents(".content").css("marginTop", parseInt(badMarginTop) - parseInt($(this).attr("height"))/2 + "px");
+$(document).ready(function() {
+  $(".content img").each(function() {
+    var badMarginTop = $(this).parents(".content").css("marginTop");
+    $(this).parents(".content").css("marginTop", parseInt(badMarginTop) - parseInt($(this).attr("height"))/2 + "px");
+  });
 });
-
